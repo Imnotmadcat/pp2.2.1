5881762cc33d06b363bdfdd30c3286c13d40841a
Imnotmadcat <Imnotmadcat@gmail.com>
2022-08-12 17:10:16 +0600

    убран cascade из car.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -13,9 +13,7 @@ public class MainApp {
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
-
       UserService userService = context.getBean(UserService.class);
-
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -17,10 +17,6 @@ public class Car {
     @Column(name = "series")
     private int series;
 
-    @OneToOne(cascade = CascadeType.ALL,
-    mappedBy = "car")
-    private User user;
-
     public Car() {
 
     }
@@ -54,13 +50,7 @@ public class Car {
         this.series = series;
     }
 
-    public User getUser() {
-        return user;
-    }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
 
     @Override
     public String toString() {
